13ea42e8086ee21b30c45f179f0ab50cd72ebe26
Tyler Pewarski <tylerpewarski@Tylers-MBP.fios-router.home>
2019-04-19 10:15:10 -0400

    Fixed iex request

--- services/analysis/tools/spyNormilization.js
+++ services/analysis/tools/spyNormilization.js
@@ -1,21 +1,28 @@
+require('isomorphic-fetch');
 const { getRevenueData, getPriceData } = require('../../helpers/IEXHelpers');
 
-const spyNormilization = async (symbol, fincialDataKey, period, dateRange) => {
-    //get prices
-    const priceSeries = await getPriceData(symbol, '1y');
-    console.log('priceSeries', priceSeries);
-    //get financial data
-    const revenue = await getRevenueData(symbol);
-    console.log('revenue', revenue);
-    //generate multiple data
-    //generate heatMap
+const spyNormilization = async (symbol, period, dateRange) => {
+    try {
+        // //get financial data
+        // const revenue = await getRevenueData(symbol);
+        // console.log('revenue', revenue);
+        //get prices
+        const priceSeries = await getPriceData(symbol);
+        console.log('priceSeries', priceSeries);
+        //generate multiple data
+        //generate heatMap
 
-    return {
-        //[{date: date, value: price}]
-        priceData: {},
-        //[{date: date, value: multiple}]
-        multipleData: {},
-        //[{bucket: bucket, value: frequency}]
-        heatMap: {}
-    };
+        return {
+            //[{date: date, value: price}]
+            priceData: {},
+            //[{date: date, value: multiple}]
+            multipleData: {},
+            //[{bucket: bucket, value: frequency}]
+            heatMap: {}
+        };
+    } catch (e) {
+        console.log('ERROR: ', e);
+    }
 };
+
+spyNormilization('aapl');
